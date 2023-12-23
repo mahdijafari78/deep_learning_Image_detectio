8430f1fd86545462a260607aab673be3d3e5b7e0
mahdi jafari <88431887+mahdijafari78@users.noreply.github.com>
2023-12-23 20:34:11 +0330

    start vgg16

---
+++ VGG16.py
@@ -0,0 +1,24 @@
+import time
+
+from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+image_folder_path = "image"
+
+image_names = os.listdir(image_folder_path)
+model = VGG16(weights='imagenet')
+
+for image_name in image_names:
+    img = image.load_img(f'{image_folder_path}/{image_name}', target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    predictions = model.predict(img_array)
+    decoded_predictions = decode_predictions(predictions)[0]
+    # print(f"name_image: {image_name}")
+    plt.imshow(img)
+    plt.title(f"name_image: {image_name} || predictions:{decoded_predictions[0][1]} ")
+    plt.show()
+    time.sleep(5)
